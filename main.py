760085e896ee99088eaf14e0c772ca9c109a15a1
Emmanuel Castillo <>
2021-05-24 19:51:24 +0000

    1.0

--- main.py
+++ main.py
@@ -5,24 +5,39 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-first_name_l1 = name1.lower().count("t") 
-first_name_l2 = name1.lower().count("r")
-first_name_l3 = name1.lower().count("u")
-first_name_l4 = name1.lower().count("e")
+first_name = name1.lower()
+second_name = name2.lower()
 
-sum_first_name = first_name_l1 + first_name_l2 + first_name_l3 + first_name_l4
+true_two_name = (first_name + second_name)
+true_1 = int(true_two_name.count("t"))
+true_2 = int(true_two_name.count("r"))
+true_3 = int(true_two_name.count("u"))
+true_4 = int(true_two_name.count("e"))
 
-second_name_l1 = name2.lower().count("l")
-second_name_l2 = name2.lower().count("o")
-second_name_l3 = name2.lower().count("v")
-second_name_l4 = name2.lower().count("e")
+true_name_last = (true_1+true_2+true_3+true_4)
 
-sum_second_name = second_name_l1 + second_name_l2 + second_name_l3 + second_name_l4
+love_two_name = (first_name + second_name)
+love_1 = int(love_two_name.count("l"))
+love_2 = int(love_two_name.count("o"))
+love_3 = int(love_two_name.count("v"))
+love_4 = int(love_two_name.count("e"))
 
-join_part1 = str(sum_first_name)
-join_part2 = str(sum_second_name)
+love_name_last = (love_1+love_2+love_3+love_4)
+
+join_part1 = str(true_name_last)
+join_part2 = str(love_name_last)
 join_finish = (join_part1 + join_part2)
 
 join_convert = int(join_finish)
 
-print(join_finish)
+if join_convert <10:
+  print(f"Your score is {join_convert}%, you go together like coke and mentos.")
+elif join_convert >90:
+  print(f"Your score is {join_convert}%, you go together like coke and mentos.")
+elif join_convert ==40:
+  print(f"Your score is {join_convert}%, you are alright together.")
+elif join_convert <=50:
+  print(f"Your score is {join_convert}%, you are alright together.")
+else:
+  print(F"Your score is {join_convert}%.")
+
