e263b8964ffeb02cb55f155a462ed58f3743f77d
Emmanuel Castillo <>
2021-05-24 16:06:19 +0000

    Starting point.

--- main.py
+++ main.py
@@ -5,5 +5,18 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+first_name_l1 = name1.lower().count("t") 
+first_name_l2 = name1.lower().count("r")
+first_name_l3 = name1.lower().count("u")
+first_name_l4 = name1.lower().count("e")
 
+sum_first_name = first_name_l1 + first_name_l2 + first_name_l3 + first_name_l4
 
+second_name_l1 = name2.lower().count("l")
+second_name_l2 = name2.lower().count("o")
+second_name_l3 = name2.lower().count("v")
+second_name_l4 = name2.lower().count("e")
+
+sum_second_name = second_name_l1 + second_name_l2 + second_name_l3 + second_name_l4
+
+if sum_first_name and sum_second_name
